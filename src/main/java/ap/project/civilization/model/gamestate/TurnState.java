57a046bddeb7e5c072daa810b429fd29b199e7b4
Hamed Mirzaei <hmirzaei.tech@gmail.com>
2026-07-15 00:11:02 +0330

    Add: next turn confirmation

--- src/main/java/ap/project/civilization/model/gamestate/TurnState.java
+++ src/main/java/ap/project/civilization/model/gamestate/TurnState.java
@@ -2,6 +2,10 @@ package ap.project.civilization.model.gamestate;
 
 import ap.project.civilization.model.world.hex.HexManager;
 import ap.project.civilization.model.world.unit.UnitManager;
+import ap.project.civilization.model.world.unit.core.Unit;
+import ap.project.civilization.model.world.unit.core.UnitType;
+import ap.project.civilization.model.world.unit.units.Worker;
+import ap.project.civilization.view.navigation.components.ConfirmationDialog;
 
 public class TurnState {
     private static TurnState instance;
@@ -25,8 +29,24 @@ public class TurnState {
         return turn;
     }
     public void nextTurn() {
+        if(!unprocessedUnits()) return;
+
         System.out.println("next turn");
         turnResolve.resolveTurn();
-        turn++; // must be the last statement
+        turn++;
+    }
+
+    public boolean unprocessedUnits() {
+        for(Unit unit : UnitManager.getInstance().getUnits()) {
+            if(unit.getType() == UnitType.WORKER) {
+                if(!((Worker)unit).isEmployed()) {
+                    return ConfirmationDialog.show(null, "un-employed workers. continue?");
+                }
+            }
+            if(unit.getAp() > 0) {
+                return ConfirmationDialog.show(null, "units have ap. continue?");
+            }
+        }
+        return true;
     }
 }
