b3a6e222acc0285257d834d6732cde47bad4444d
Hamed Mirzaei <hmirzaei.tech@gmail.com>
2026-09-07 13:07:05 +0330

    phase3 release

--- src/main/java/ap/project/civilization/network/connection/ServerSocketManager.java
+++ src/main/java/ap/project/civilization/network/connection/ServerSocketManager.java
@@ -1,32 +1,51 @@
 package ap.project.civilization.network.connection;
 
-import java.io.BufferedReader;
+
+import ap.project.civilization.network.server.GameServer;
+
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class ServerSocketManager {
-    static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(5000);
+public class ServerSocketManager implements Runnable {
+    private final int port;
+    private final GameServer gameServer;
 
-        System.out.println("Server started");
-        Thread listener = new Thread(() -> {
+    private ServerSocket serverSocket;
 
-        });
-        Socket socket = serverSocket.accept();
-        System.out.println("client connected");
+    public ServerSocketManager(int port, GameServer gameServer) {
+        this.port = port;
+        this.gameServer = gameServer;
+    }
 
-        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    @Override
+    public void run() {
+        try {
+            serverSocket = new ServerSocket(port);
 
-        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            System.out.println("Server listening on port " + port);
 
-        String message = in.readLine();
-        System.out.println("client said: " + message);
-        out.println("hello from server");
+            while(!Thread.currentThread().isInterrupted()) {
+                Socket socket = serverSocket.accept();
+                System.out.println("New connection: " + socket.getInetAddress());
 
-        socket.close();
-        serverSocket.close();
+                gameServer.handleNewConnection(socket);
+            }
+
+        } catch (IOException e) {
+            if(!Thread.currentThread().isInterrupted()) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void stop() {
+        try {
+            if(serverSocket != null) {
+                serverSocket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/ap/project/civilization/network/server/GameServer.java
+++ src/main/java/ap/project/civilization/network/server/GameServer.java
@@ -1,5 +1,30 @@
 package ap.project.civilization.network.server;
 
+import ap.project.civilization.network.connection.ServerSocketManager;
+
+import java.net.Socket;
+
 public class GameServer {
-    
+    private static final int PORT = 5000;
+
+    private final ServerSocketManager serverSocketManager;
+    private final Thread socketThread;
+
+    public GameServer() {
+        serverSocketManager = new ServerSocketManager(PORT, this);
+        socketThread = new Thread(serverSocketManager, "ServerSocketThread");
+    }
+
+    public void start() {
+        socketThread.start();
+        System.out.println("Game Server Started.");
+    }
+
+    public void stop() {
+        serverSocketManager.stop();
+    }
+
+    public void handleNewConnection(Socket socket) {
+
+    }
 }
